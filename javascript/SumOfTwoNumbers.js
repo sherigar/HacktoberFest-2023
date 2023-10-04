0d25d92c97d2abd1a8d1e938b6e0a09ad4032044
Devnashu Patil <devanshupatil692@gmail.com>
2023-10-05 01:42:18 +0530

    jump game in c

--- javascript/SumOfTwoNumbers.js
+++
@@ -1,6 +0,0 @@
-// I would take two parameters and the following function can be used to calculate the sum of any 2 numbers that are passed as arguments. 
-function sumOfTwoNumbers(a, b) { 
-
-    return a + b; 
-  
-  } 
