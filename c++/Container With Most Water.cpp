935a00f3025a1867293ad3ac77c77e8fa91fce97
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-10-19 01:58:40 +0530

    Create Container With Most Water.cpp
    Kindly merge my contribution as I have solved the container with most water problem in C++ language successfully.
    Thank!@sherigar
    Issue #154

---
+++ c++/Container With Most Water.cpp
@@ -0,0 +1,35 @@
+// Contributed by:
+Name: Abhijai Rajawat
+University: VIT, Vellore 
+
+// Problem Statement:
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+
+// Problem Solution:
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int water = 0;
+        int i = 0, j = height.size() - 1;
+        while (i < j) {
+            int h = min(height[i], height[j]);
+            water = max(water, (j - i) * h);
+            while (height[i] <= h && i < j) i++;
+            while (height[j] <= h && i < j) j--;
+        }
+        return water;
+    }
+};
