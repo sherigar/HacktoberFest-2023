74149bf5864706ac35c78d3c6338207887931ae8
thevineetdixit <148352948+thevineetdixit@users.noreply.github.com>
2023-10-18 19:27:18 +0530

    Create sum.cpp

---
+++ c++/sum.cpp
@@ -0,0 +1,17 @@
+ #include<iostream> 
+ using namespace std;
+
+ int main()
+ {
+
+    int num1,num2;
+    cout<<"enter the value of num 1:\n";/*<< is called insertion operator */
+
+    cin>>num1; /*>> is called extraction operator*/
+
+    cout<<"enter the value of num2:\n";
+    cin>>num2;
+
+    cout<<"the sum is "<< num1+num2;
+    return 0;
+ }
