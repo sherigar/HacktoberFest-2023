ab8080b7edb36770d2a8448857bad8505723cdd0
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-10-19 12:11:50 +0530

    Create Celsius To Fahrenheit.py
    Solved the celsius To Fahrenheit problem using python language.
    Kindly merge my contribution. Happy to contribute.
    Thank!

---
+++ python/Celsius To Fahrenheit.py
@@ -0,0 +1,16 @@
+# Contributed by: 
+Name: Abhijai Rajawat
+University: VIT, Vellore
+
+# Problem Statement:
+Write a python program to convert celsius To Fahrenheit by taking input from user
+
+# Problem Solution:
+# Get the temperature in Celsius from the user
+celsius = float(input("Enter the temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Display the result
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
