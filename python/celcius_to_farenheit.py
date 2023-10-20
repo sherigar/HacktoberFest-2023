644a874bb03e7b907e895396fb15989d731fafe7
Mr-krishn <krishnakumar13th@gmail.com>
2023-10-21 00:46:12 +0530

    Temperature conversion from celsius to fahrenheit is successfully commited

---
+++ python/celcius_to_farenheit.py
@@ -0,0 +1,11 @@
+# Introduction
+print("Hello, I am Krishna kumar, Studied in GGSIPU Main campus Dwarka, Delhi")
+
+#taking input from user
+celsius = float(input("Enter the Temperature in Celsius : "))
+
+# formula used 
+fahrenheit = (1.8 * celsius) + 32
+
+#final output
+print(celsius,"degree celsius will be", fahrenheit,"degree fahrenheit.")
