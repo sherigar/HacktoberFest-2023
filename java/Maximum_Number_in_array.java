3685307af1c7b1d3db479277514d92e3b0e1c9e8
Rajiv Paul <74105638+Rajiv-paul@users.noreply.github.com>
2023-11-02 01:16:08 +0530

    Update Maximum_Number_in_array.java

--- java/Maximum_Number_in_array.java
+++ java/Maximum_Number_in_array.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Maximum_Number_in_array {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
